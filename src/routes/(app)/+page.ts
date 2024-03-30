c780ecf01d7cdfba09b43e91e05c61f3b33c655e
Gunnar Omander <gunnar.omander@gmail.com>
2024-03-30 12:58:19 +0100

    Fix error in previous commit

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,11 +1,11 @@
-import openaiModels from '$lib/data/models/openai'
-import { Api, type ApiType, type Message } from '$types/common'
+import models from '$lib/data/models'
+import { Api } from '$types/common'
 import type { LoadData } from './types'
 
 export function load(): LoadData {
   return {
     messages: [],
     api: Api.OPENAI,
-    model: openaiModels.default.name
+    model: models.openai.default.name
   }
 }
