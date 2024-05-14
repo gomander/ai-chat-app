ff78d438518ddedc3d48318a5850189d5a9d4ac0
Gunnar Omander <gunnar.omander@gmail.com>
2024-05-14 23:05:14 +0200

    Markdown!

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
+  import { parse } from 'marked'
+  import DOMPurify from 'dompurify'
   import type { ApiMessage } from '$types/common'
 
   let { content, role }: ApiMessage = $props()
+
+  const dirtyHtml = parse(content)
 </script>
 
 <div
   class="relative wrapper-{role}"
   aria-label="{role} message"
 >
-  <div class="p-3 mx-2 rounded-2xl max-w-prose message message-{role}">
-    {content}
+  <div class="p-3 mx-2 rounded-2xl max-w-prose flex flex-col gap-2 overflow-x-auto message message-{role}">
+    {#await dirtyHtml then htmlContent}
+      {@html DOMPurify.sanitize(htmlContent)}
+    {/await}
   </div>
 </div>
 
@@ -18,9 +24,35 @@
     align-self: flex-end;
   }
 
-  .message {
-    overflow-wrap: anywhere;
-    white-space-collapse: preserve;
+  .message :global(pre) {
+    background-color: rgb(var(--color-surface-700));
+    color: rgb(var(--on-surface));
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    overflow-x: auto;
+  }
+
+  .message :global(pre > code) {
+    background-color: unset;
+    padding: 0;
+    border-radius: 0;
+  }
+
+  .message :global(code) {
+    background-color: rgb(var(--color-surface-700));
+    color: rgb(var(--on-surface));
+    padding: 0.25rem;
+    border-radius: 0.25rem;
+  }
+
+  .message :global(ul) {
+    list-style-type: disc;
+    padding-left: 1rem;
+  }
+
+  .message :global(ol) {
+    list-style-type: decimal;
+    padding-left: 1rem;
   }
 
   .message::after {
